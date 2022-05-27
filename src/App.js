b19d16ef7ababc7c79240a9d5076cf6e66cad935
jinhlee19 <jinhlee19@gmail.com>
2022-05-27 19:32:27 +0900

    OR 연산자 에러처리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
+const name = '리액트';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// const name1 = undefined;
+	const name1 = 'REACT';
+	return (
+		<>
+			{name === '리액트' && <h1>리액트 안녕</h1>}
+			<h2>{name1 || '리액트'}</h2>
+		</>
+	);
 }
 
 export default App;
