2a749f1ddb69652d6bb07cddf46d2b921479f67e
jinhlee19 <jinhlee19@gmail.com>
2022-06-05 23:26:55 +0900

    6.4.2. 데이터 추가기능 구현하기

--- src/components/Section641.jsx
+++ src/components/Section641.jsx
@@ -10,7 +10,25 @@ const Section641 = () => {
 	const [inputText, setInputText] = useState('');
 	const [nextId, setNextId] = useState(5);
 	const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
-	return <div>{namesList}</div>;
+
+	const onChange = e => setInputText(e.target.value);
+	const onClick = () => {
+		const nextNames = names.concat({
+			id: nextId,
+			text: inputText,
+		});
+		setNextId(nextId + 1);
+		setNames(nextNames);
+		setInputText('');
+	};
+
+	return (
+		<>
+			<input type="text" value={inputText} onChange={onChange} />
+			<button onClick={onClick}>추가</button>
+			<ul>{namesList}</ul>
+		</>
+	);
 };
 
 export default Section641;
